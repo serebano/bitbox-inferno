27c15a6ba2277e22abcd20056e425ea9f6401859
Sergiu Toderascu <sergiu.toderascu@gmail.com>
2017-05-22 06:07:24 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import bitbox from "bitbox"
-import createComponent from "./box"
-
+import inferno from "./box"
 import App from "./App"
 import appStore from "./store"
-
 import { render } from "inferno"
-
 import "./index.css"
 import "inferno-devtools"
 
 const store = bitbox(appStore)
+
 const foo = bitbox({ count: 1 })
 const bar = bitbox({ count: 2 })
+
+const counter = {}
+// bitbox(
+//     console.log,
+//     function Counter(props) {
+//         return (
+//             <section>
+//                 <h1>{props.count}</h1>
+//                 <button onClick={props.inc}>inc</button>
+//             </section>
+//         )
+//     },
+//     foo
+// )
+
 bitbox.count(
     {
         foo,
@@ -39,12 +52,17 @@ bitbox(
 
 bitbox(() => console.log(`${foo.count} | ${bar.count}`))
 
-Object.assign(window, { bitbox, store })
+Object.assign(window, { bitbox, store, counter })
 
-store.date = bitbox(date => {
-    render(<div>{date.now}</div>, document.getElementById("time"))
-    setTimeout(() => (date.now = Date.now()))
+// obj = { count: 0 }
+// app = bitbox.count(obj, console.warn)
+// app.count++
+
+const box = bitbox.count(0, count => {
+    render(<h1>{count}</h1>, document.getElementById("time"))
 })
+
+box.int = setInterval(() => (box.count < 100 ? box.count++ : clearInterval(box.int)))
 
 render(<App store={store} />, document.getElementById("app"))
 
